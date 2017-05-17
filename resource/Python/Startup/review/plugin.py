4399be5caa238aa9b9fa6fbfcd84cf282dad674b
Mattias Lagergren <mattias.lagergren@ftrack.com>
2017-05-17 15:44:58 -0700

    Update code style

--- resource/Python/Startup/review/plugin.py
+++ resource/Python/Startup/review/plugin.py
@@ -191,7 +191,7 @@ class Plugin(QObject):
             self._componentPathCache[componentId] = path
 
             self.logger.debug(
-                u'Found path :{0} for component :{1} in location :{2}'.format(
+                u'Found path: {0} for component: {1} in location: {2}'.format(
                     path, ftrack_component, location
                 )
             )
